380fe0d7a73569db9334620c1925b769893e6890
Ashwin.D.S <astroashwin@outlook.com>
2021-07-07 14:31:09 +0400

    ROM Tracker: schedule and connect to TG
    Signed-off-by: Ashwin.D.S <astroashwin@outlook.com>

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import os
+os.system("sudo pip install pyTelegramBotAPI")
+import telebot
+
+
+def send_mes(text):
+    if text == "":
+        text = " "
+    return bot.send_message(CHAT_ID, text)
 
 GH_TOKEN = ""
 BOT_API = os.environ.get("BOT_API")
@@ -6,9 +14,12 @@ CHAT_ID = os.environ.get("CHAT_ID")
 GH_TOKEN = os.environ.get("GH_TOKEN")
 
 
-print(BOT_API)
+bot = telebot.TeleBot(BOT_API, parse_mode="MARKDOWN")
+
 print(CHAT_ID)
 if GH_TOKEN == "":
     print("Token not found")
 else:
     print("Token found")
+
+send_mes("It is working")
