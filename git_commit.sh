bd4f46b37f0b254518e35df3751846d9f3f7453c
Ashwin.D.S <astroashwin@outlook.com>
2021-07-08 23:00:42 +0400

    ROM Tracker: add gitconfigs

--- git_commit.sh
+++ git_commit.sh
@@ -1,3 +1,5 @@
+git config --global user.email "<>"
+git config --global user.name "GitHub Actions Bot"
 git add .
 git commit -m "ROM Tracker:Update SHAs [BOT]"
-git push https://geek0609:${{ secrets.GITHUB_TOKEN }}@github.com/geek0609/rom-tracker new-implementation
+git push origin new-implementation
