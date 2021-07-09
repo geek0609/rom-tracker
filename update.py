64d9aa9432491e32621a54217da47f710d64f628
Ashwin.D.S <astroashwin@outlook.com>
2021-07-09 19:48:45 +0400

    Update Workflow: Add date and time of update

--- update.py
+++ update.py
@@ -1,3 +1,4 @@
+import datetime
 readme = open("Tracked_ROMs.MD", "w+")
 content = "# The list of Manifest Repo of the ROMs that are tracked" \
           "\n\n*The list is automatically updated*" \
@@ -9,6 +10,8 @@ roms = open("roms.txt", "r")
 
 for rom in roms.readlines():
     content = content + " * [" + rom.strip("\n") + "](https://github.com/" + rom.strip("\n") + ")\n"
+
+content = content + "\n\nLast updated on : " + str(datetime.datetime.utcnow()) + " UTC"
 
 readme.write(content)
 readme.close()
