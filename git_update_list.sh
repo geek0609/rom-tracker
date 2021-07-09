429a0d95f58e1fc0ca0745c2f4840f71bae7b2c6
Ashwin.D.S <astroashwin@outlook.com>
2021-07-09 18:28:16 +0400

    ROM Tracker: Add workflow to keep updating list of ROMs

---
+++ git_update_list.sh
@@ -0,0 +1,5 @@
+git config --global user.email "<>"
+git config --global user.name "GitHub Actions Bot"
+git add .
+git commit -m "ROM Tracker: Update List [BOT]"
+git push origin new-implementation
---
+++ update.py
@@ -0,0 +1,15 @@
+readme = open("Tracked_ROMs.MD", "w+")
+content = "# The list of Manifest Repo of the ROMs that are tracked" \
+          "\n\n*The list is automatically updated*" \
+          "\n\n*Project is still work in progress*" \
+          "\n\n### Telegram Channel : [Click Me](https://t.me/ROM_tracker)" \
+          "\n## ROMs:\n"
+
+roms = open("roms.txt", "r")
+
+for rom in roms.readlines():
+    content = content + " * [" + rom.strip("\n") + "](https://github.com/" + rom.strip("\n") + ")\n"
+
+readme.write(content)
+readme.close()
+roms.close()
