a064c74b2237377a2c4b05d236d39e73e44fa7ce
Ashwin.D.S <astroashwin@outlook.com>
2021-07-11 13:39:08 +0400

    RT: Now show numbers in list

--- update.py
+++ update.py
@@ -8,16 +8,18 @@ content = "# The list of Manifest Repo of the ROMs that are tracked" \
           "\n## ROMs:\n"
 
 roms = open("roms.txt", "r")
-
+number = 1
 for rom in roms.readlines():
-    content = content + " * [" + rom.strip("\n") + "](https://github.com/" + rom.strip("\n") + ")\n"
+    content = content + str(number) + ". [" + rom.strip("\n") + "](https://github.com/" + rom.strip("\n") + ")\n"
+    number += 1
 
 roms1 = open("roms1.txt", "r")
 
 for rom in roms1.readlines():
-    content = content + " * [" + rom.strip("\n") + "](https://github.com/" + rom.strip("\n") + ")\n"
+    content = content + str(number) + ". [" + rom.strip("\n") + "](https://github.com/" + rom.strip("\n") + ")\n"
+    number += 1
 
-content = content + "\n\nLast updated on : " + str(datetime.datetime.utcnow()) + " UTC"
+content = content + "\n### Total number of repos tracked: " + str(number) +"\n\nLast updated on : " + str(datetime.datetime.utcnow()) + " UTC"
 
 readme.write(content)
 readme.close()
