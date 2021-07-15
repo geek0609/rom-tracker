93d9a14eac7619d05a326e611992a6462ad064ca
Ashwin.D.S <astroashwin@outlook.com>
2021-07-15 20:46:25 +0400

    RT: Rename BOT and pull new commits before pushing commit
    nothing to commit, working tree clean
    To https://github.com/geek0609/rom-tracker
     ! [rejected]        new-implementation -> new-implementation (fetch first)
    error: failed to push some refs to 'https://github.com/geek0609/rom-tracker'
    hint: Updates were rejected because the remote contains work that you do
    hint: not have locally. This is usually caused by another repository pushing
    hint: to the same ref. You may want to first integrate the remote changes
    hint: (e.g., 'git pull ...') before pushing again.
    hint: See the 'Note about fast-forwards' in 'git push --help' for details.
    Error: Process completed with exit code 1.
    
    Signed-off-by: Ashwin.D.S <astroashwin@outlook.com>

--- git_commit.sh
+++ git_commit.sh
@@ -1,5 +1,7 @@
 git config --global user.email "<>"
-git config --global user.name "GitHub Actions Bot"
+git config --global user.name "ROM Tracker BOT"
+git fetch
+git pull
 git add .
-git commit -m "ROM Tracker:Update SHAs [BOT]"
+git commit -m "ROM Tracker: Update SHAs [BOT]"
 git push origin new-implementation
