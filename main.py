94cac406335a9c4a42ae4bc59cd3b6681c7e3289
Ashwin.D.S <astroashwin@outlook.com>
2021-07-22 16:40:57 +0530

    RT: Add Copyright info

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+#!/usr/bin/env python
+#
+# Python code which automatically sends a Message in a Telegram Channel if any new commit is found in the specified repos
+# See README.MD for more
+#
+# Copyright (C) 2021 Ashwin DS <astroashwin@outlook.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation;
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+
 import os
 import telebot
 import sys
