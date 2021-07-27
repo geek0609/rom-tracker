7231bf9e372c84e058126c9133ddebfa08df31fa
Sigmanificient <edhyjox@gmail.com>
2021-07-27 13:02:47 +0200

    improved main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #
-# Python code which automatically sends a Message in a Telegram Channel if any new commit is found in the specified repos
-# See README.MD for more
+# Python code which automatically sends a Message in a Telegram Channel if
+# any new commit is found in the specified repos See README.MD for more
 #
 # Copyright (C) 2021 Ashwin DS <astroashwin@outlook.com>
 #
@@ -27,102 +27,125 @@ import random
 
 
 def send_mes(text):
-    if text == "":
-        text = "NO/EMPTY TEXT GIVEN TO SEND"
-    return bot.send_message(CHAT_ID, text, disable_web_page_preview=True)
+    return bot.send_message(CHAT_ID, text or "NO/EMPTY TEXT GIVEN TO SEND")
+
+
 # Send a message
 
-timeout = random.randint(0,60)
+timeout = random.randint(0, 60)
 
 for i in range(timeout):
-    print ("Waiting for " + str(timeout) + " before starting, this random wait helps avoid conflicts during push at same time\n\nTime remaining : " + str(timeout - i))
-    time.sleep(1)
+    print(
+        f"Waiting for {timeout}s before starting, this random wait helps avoid "
+        f"conflicts during push at same time\n\nTime remaining : {timeout - i} "
+    )
 
 # Init telegram bot
-BOT_API = sys.argv[2]
 CHAT_ID = sys.argv[1]
+BOT_API = sys.argv[2]
 REPO_LIST = sys.argv[3]
-bot = telebot.TeleBot(BOT_API, parse_mode="HTML")
+bot = telebot.TeleBot(BOT_API)
 
 
 def update(repo, converted):
     file_name = repo.replace("/", "_") + ".txt"
     # Sends a request to github api to get the latest commits
-    # req = requests.get("https://api.github.com/repos/" + repo + "/commits").content
+    # req = requests.get(f"https://api.github.com/repos/{repo}/commits").content
     # converted = json.loads(req)
-    shas = []
-    for x in converted:
-        shas.append(x['sha'])
+    sha_list = '\n'.join(map(str, (x['sha'] for x in converted)))
+
     if os.path.isfile(file_name):
         os.remove(file_name)
+
     with open(f'roms/{file_name}', "w+") as f:
-        for s in shas:
-            f.write(str(s) + "\n")
+        f.write(sha_list)
+
     return True
 
 
-def get_diff (repo, converted):
+def get_diff(repo, converted):
     file_name = repo.replace("/", "_") + ".txt"
-    # req = requests.get("https://api.github.com/repos/" + repo + "/commits").content
+
+    # req = requests.get(f"https://api.github.com/repos/{repo}/commits").content
     # converted = json.loads(req)
+
     # Fetches new commits (if any) from API
-    shas = []
-    for x in converted:
-        shas.append(x['sha'])
+    sha_list = [x['sha'] for x in converted]
+
     # Reads previous commits from where it is stored
-    prev_sha = []
-    file = open(file_name, "r")
-    for line in file.readlines():
-        prev_sha.append(line.replace("\n", ""))
-    print(shas)
+    with open(file_name) as f:
+        prev_sha = [line.replace("\n", "") for line in f.read().splitlines()]
+
+    print(sha_list)
     print(prev_sha)
-    if shas == prev_sha:
+
+    if sha_list == prev_sha:
         return False
-    first_set = set(shas)
-    sec_set = set(prev_sha)
-    differences = first_set - sec_set
+
+    differences = set(sha_list) - set(prev_sha)
+    print(differences)
+
     # TODO: Remove this useless logic
-    if len(list(differences)) == 0:
-        print("No change")
-        return False
-    else:
+
+    if len(list(differences)) != 0:
         return list(differences)
+    print("No change")
+
+    return False
 
 
-# Start of actual code
-file = open(REPO_LIST, "r")
-repo = file.readlines()
-# Read roms from roms.txt
-for rom in repo:
-    rom = rom.replace("\n", "")
+def send_rom(rom):
     file_name = rom.replace("/", "_") + ".txt"
-    req = requests.get("https://api.github.com/repos/" + rom + "/commits").content
+
+    req = requests.get("https://api.github.com/repos/{rom}/commits").content
     converted = json.loads(req)
+
     if os.path.isfile(file_name):
-        print(file_name + " Exists")
+        print(file_name, "Exists")
+
     else:
-        # In case the file is just added to the list, it needs to be updated first,
+        # In case the file is just added to the list,
+        # it needs to be updated first,
         # before it can compare for new commits
         print("Not exist")
         update(rom, converted)
+
     # Check if new commit
     result = get_diff(rom, converted)
-    # I can use if result but i choose to do this for sake of my understanding
-    if result != False:
-        # need to be in this format <a href="http://www.example.com/">inline URL</a>
-        message = "New commit(s) in \n" + "<a href=\"" + "https://github.com/" + str(rom) + "\">" + str(rom) + "</a>\n\n\n"
-        number = 1
-        result.reverse() # Else it shows way older commits first
-        for commit in result:
-            if number <= 10:
-                # need to be in this format <a href="http://www.example.com/">inline URL</a>
-                message = message + "<a href=\"" + "https://github.com/" + str(rom) + "/commit/" + str(commit) + "\">Click me for commit " + str(number) + "</a>\n"
-                number = number + 1
-            else:
-                break
-        send_mes(message)
-        time.sleep(30)
-        # to not spam api 
-        update(rom, converted)
-    else:
-        print("All up to date") # For Logging purposes
+
+    if not result:
+        print("All up to date")  # For Logging purposes
+        return
+
+    # need to be in this format :
+    # <a href="http://www.example.com/">inline URL</a>
+
+    result.reverse()  # Else it shows way older commits first
+    message = f"New commit(s) in \n<a href=\"https://github.com/{rom}\">{rom}</a>\n\n\n"
+
+    for number, commit in enumerate(result):
+        if number > 10:
+            break
+
+    # need to be in this format :
+    # <a href="http://www.example.com/">inline URL</a>
+    message += f"<a href=\"https://github.com/{rom}/commit/{commit}\">Click me for commit {number}</a>\n"
+
+    send_mes(message)
+    time.sleep(30)
+    # to not spam api
+    update(rom, converted)
+
+
+# Start of actual code
+def main():
+    with open(REPO_LIST) as f:
+        repo = f.read().splitlines()
+
+    # Read roms from roms.txt
+    for rom in repo:
+        send_rom(rom)
+
+
+if __name__ == '__main__':
+    main()
