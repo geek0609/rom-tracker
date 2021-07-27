51ba94dacf0d354a52dab1c20fbcaa8d3d4db639
Sigmanificient <edhyjox@gmail.com>
2021-07-27 11:15:55 +0200

    simplifying update.py

--- update.py
+++ update.py
@@ -1,16 +1,23 @@
 import datetime
+
+with open("roms.txt", "r") as f:
+    roms = f.read().splitlines()
+
+tracked_list = '\n'.join(
+    f"{i}. [{rom}](https://github.com/{rom})" for i, rom in enumerate(roms)
+)
+
+content = '\n\n'.join(
+    (
+        '# The list of Repos of the ROMs that are tracked',
+        '*The list is automatically updated*',
+        '### Telegram Channel : [Click Me](https://t.me/ROM_tracker)',
+        '## ROMs:',
+        tracked_list,
+        f'### Total number of repos tracked: {len(roms)}',
+        f"Last updated on : {datetime.datetime.utcnow()} UTC"
+    )
+)
+
 with open("Tracked_ROMs.MD", "w+") as readme:
-    # W+ to rewrite entire list everytime
-    content = "# The list of Repos of the ROMs that are tracked" \
-              "\n\n*The list is automatically updated*" \
-              "\n\n### Telegram Channel : [Click Me](https://t.me/ROM_tracker)" \
-              "\n## ROMs:\n"
-
-    with open("roms.txt", "r") as roms:
-        number = 1
-        for rom in roms.readlines():
-            content = content + str(number) + ". [" + rom.strip("\n") + "](https://github.com/" + rom.strip("\n") + ")\n"
-            number += 1
-
-    content = content + "\n### Total number of repos tracked: " + str(number-1) +"\n\nLast updated on : " + str(datetime.datetime.utcnow()) + " UTC"
     readme.write(content)
