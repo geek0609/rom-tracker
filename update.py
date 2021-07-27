a712f0c6890e0c2d246225f283a324dfa80869bc
Sigmanificient <edhyjox@gmail.com>
2021-07-27 11:02:52 +0200

    moving every tracked rooms to a single file.

--- update.py
+++ update.py
@@ -1,43 +1,16 @@
 import datetime
-readme = open("Tracked_ROMs.MD", "w+")
-# W+ to rewrite entire list everytime
-content = "# The list of Repos of the ROMs that are tracked" \
-          "\n\n*The list is automatically updated*" \
-          "\n\n### Telegram Channel : [Click Me](https://t.me/ROM_tracker)" \
-          "\n## ROMs:\n"
+with open("Tracked_ROMs.MD", "w+") as readme:
+    # W+ to rewrite entire list everytime
+    content = "# The list of Repos of the ROMs that are tracked" \
+              "\n\n*The list is automatically updated*" \
+              "\n\n### Telegram Channel : [Click Me](https://t.me/ROM_tracker)" \
+              "\n## ROMs:\n"
 
-roms = open("roms.txt", "r")
-number = 1
-for rom in roms.readlines():
-    content = content + str(number) + ". [" + rom.strip("\n") + "](https://github.com/" + rom.strip("\n") + ")\n"
-    number += 1
+    with open("roms.txt", "r") as roms:
+        number = 1
+        for rom in roms.readlines():
+            content = content + str(number) + ". [" + rom.strip("\n") + "](https://github.com/" + rom.strip("\n") + ")\n"
+            number += 1
 
-roms1 = open("roms1.txt", "r")
-
-for rom in roms1.readlines():
-    content = content + str(number) + ". [" + rom.strip("\n") + "](https://github.com/" + rom.strip("\n") + ")\n"
-    number += 1
-
-roms2 = open("roms2.txt", "r")
-
-for rom in roms2.readlines():
-    content = content + str(number) + ". [" + rom.strip("\n") + "](https://github.com/" + rom.strip("\n") + ")\n"
-    number += 1
-
-roms3 = open("roms3.txt", "r")
-
-for rom in roms3.readlines():
-    content = content + str(number) + ". [" + rom.strip("\n") + "](https://github.com/" + rom.strip("\n") + ")\n"
-    number += 1
-
-roms4 = open("roms4.txt", "r")
-
-for rom in roms4.readlines():
-    content = content + str(number) + ". [" + rom.strip("\n") + "](https://github.com/" + rom.strip("\n") + ")\n"
-    number += 1
-
-content = content + "\n### Total number of repos tracked: " + str(number-1) +"\n\nLast updated on : " + str(datetime.datetime.utcnow()) + " UTC"
-
-readme.write(content)
-readme.close()
-roms.close()
+    content = content + "\n### Total number of repos tracked: " + str(number-1) +"\n\nLast updated on : " + str(datetime.datetime.utcnow()) + " UTC"
+    readme.write(content)
