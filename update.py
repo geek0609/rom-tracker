2866457497aae08e4f64be575f4165669a13e771
Ashwin.D.S <astroashwin@outlook.com>
2022-01-30 17:42:22 +0400

    list updater: simplify logic

--- update.py
+++ update.py
@@ -6,38 +6,21 @@ content = "# The list of Repos of the ROMs that are tracked" \
           "\n\n### Telegram Channel : [Click Me](https://t.me/ROM_tracker)" \
           "\n## ROMs:\n"
 
-roms = open("roms.txt", "r")
 number = 1
-for rom in roms.readlines():
-    content = content + str(number) + ". [" + rom.strip("\n") + "](https://github.com/" + rom.strip("\n") + ")\n"
-    number += 1
 
-roms1 = open("roms1.txt", "r")
+REPO_LIST = ["roms.txt", "roms1.txt", "roms2.txt",
+             "roms3.txt", "roms4.txt", "roms5.txt",
+             "roms6.txt", "roms7.txt", "roms8.txt"]
 
-for rom in roms1.readlines():
-    content = content + str(number) + ". [" + rom.strip("\n") + "](https://github.com/" + rom.strip("\n") + ")\n"
-    number += 1
+for LIST in REPO_LIST:
 
-roms2 = open("roms2.txt", "r")
+    roms = open(LIST, "r")
 
-for rom in roms2.readlines():
-    content = content + str(number) + ". [" + rom.strip("\n") + "](https://github.com/" + rom.strip("\n") + ")\n"
-    number += 1
-
-roms3 = open("roms3.txt", "r")
-
-for rom in roms3.readlines():
-    content = content + str(number) + ". [" + rom.strip("\n") + "](https://github.com/" + rom.strip("\n") + ")\n"
-    number += 1
-
-roms4 = open("roms4.txt", "r")
-
-for rom in roms4.readlines():
-    content = content + str(number) + ". [" + rom.strip("\n") + "](https://github.com/" + rom.strip("\n") + ")\n"
-    number += 1
+    for rom in roms.readlines():
+        content = content + str(number) + ". [" + rom.strip("\n") + "](https://github.com/" + rom.strip("\n") + ")\n"
+        number += 1
 
 content = content + "\n### Total number of repos tracked: " + str(number-1) +"\n\nLast updated on : " + str(datetime.datetime.utcnow()) + " UTC"
 
 readme.write(content)
 readme.close()
-roms.close()
